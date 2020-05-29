07fc251e9bfac5a4c9b1892cfb8a5b65fbdfbc4f
Favorit5503 <favorit5503@gmail.com>
2020-05-29 18:07:57 +0200

    Fixing emulator support for Azerothcore in the Tooltip module

--- Extended_Armory_v2.2/Content/application/modules/tooltip/controllers/tooltip.php
+++ Extended_Armory_v2.2/Content/application/modules/tooltip/controllers/tooltip.php
@@ -220,6 +220,16 @@ class Tooltip extends MX_Controller
 		// Use new query in case we support the emulator
 		switch ($this->getEmulatorString())
 		{
+			case 'azerothcore':
+				$Statement = "	SELECT 
+									entry, Flags, name, Quality, bonding, InventoryType, MaxDurability, armor, RequiredLevel, ItemLevel, class, subclass, 
+									dmg_min1, dmg_max1, dmg_type1, holy_res, fire_res, nature_res, frost_res, shadow_res, arcane_res, delay, 
+									socketColor_1, socketColor_2, socketColor_3, spellid_1, spellid_2, spellid_3, spellid_4, spellid_5, spelltrigger_1, spelltrigger_2, spelltrigger_3, spelltrigger_4, spelltrigger_5, 
+									displayid, stat_type1, stat_value1, stat_type2, stat_value2, stat_type3, stat_value3, stat_type4, stat_value4, stat_type5, stat_value5, 
+									stat_type6, stat_value6, stat_type7, stat_value7, stat_type8, stat_value8, stat_type9, stat_value9, stat_type10, stat_value10, stackable, socketBonus, itemset 
+								FROM item_template 
+								WHERE entry=?";
+				break;
 			case 'trinity':
 				$Statement = "	SELECT 
 									entry, Flags, name, Quality, bonding, InventoryType, MaxDurability, armor, RequiredLevel, ItemLevel, class, subclass, 
@@ -332,6 +342,7 @@ class Tooltip extends MX_Controller
 		switch ($this->getEmulatorString())
 		{
 			case 'trinity':
+			case 'azerothcore':
 			case 'trinity_cata':
 			case 'skyfire':
 			case 'arkcore':
@@ -1027,6 +1038,7 @@ class Tooltip extends MX_Controller
 			case 'trinity_cata':
 				return $this->getItemName($item);
 			case 'trinity':
+			case 'azerothcore':
 			case 'mangos':
 			case 'arcemu':
 				return $this->getItemName_ItemSet_Local($item);
@@ -1074,6 +1086,7 @@ class Tooltip extends MX_Controller
 		switch ($this->getEmulatorString())
 		{
 			case 'trinity':
+			case 'azerothcore':
 			case 'trinity_cata':
 			case 'arkcore':
 			case 'skyfire':
